149c80e617d6ef356b484879614159a0f717743b
Emanuelprime <emanuelll3000@gmail.com>
2024-08-21 14:22:53 -0300

    header-style

--- receitas-app/src/Components/Header.js
+++ receitas-app/src/Components/Header.js
@@ -1,18 +1,22 @@
 import { Link, useLocation } from 'react-router-dom';
 import React from 'react';
-
-import './Styles/Form.css';
+import logo from '../Assets/images/capivara.png';
+import './Styles/Header.css';
 
 export default function Header() {
     let { pathname } = useLocation();
 
     return(
         <header className="header">
+            <div className="header_title-container">
+                <img src={logo} alt="Logo" className="header_logo" />
+                <h1 className="header_title">Cozinhando com a Capivara</h1>
+            </div>
             {pathname !== "/addreceita" && (    // Renderização condicional que exibe o Link apenas quando nao esta no URL proprio
-                <Link to='/addreceita'>Nova Receita</Link>
+                <Link to='/addreceita' className="header_link">Nova Receita</Link>
             )}
             {pathname !== "/" && (              // Renderização condicional que exibe o Link apenas quando nao esta no URL proprio
-                <Link to='/'>Ver Receitas</Link>
+                <Link to='/' className="header_link">Ver Receitas</Link>
             )}
         </header>
     )
