5f06bc5563285e0ed3cf23d01b0b173c28a9980d
Joao Nakano <joaovynak@gmail.com>
2024-08-23 17:10:23 -0300

    erro do componente nao funcionando

--- receitas-app/src/Pages/Home.js
+++ receitas-app/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import './Styles/Home.css';
+import Recipe from '../Components/Recipe'
 
 export default function Recipes({ data }) {
   return (
@@ -11,7 +12,7 @@ export default function Recipes({ data }) {
             className="recipe-image"
           />
           <div className="recipe-info">
-            <h2 className="recipe-title">{objectInfo.titulo}</h2>
+            <Recipe className="recipe-title" recipeObject={objectInfo}/>
           </div>
         </div>
       ))}
@@ -19,12 +20,6 @@ export default function Recipes({ data }) {
   );
 }
 
-/*
-    Anotacoes que achei relevante
-    1. Podemos passar um state ao <Link /> que armazenara o objeto que quisermos
-    2. Em uma arrow function, podemos utilizar () ao inves de {} na expressao () => {}, ficando assim () => (), removendo-se a obrigatoriedade de um return
-    3. Para conseguir usar um template literal em uma tag JSX, usamos {}
-*/
 /*
     Anotacoes que achei relevante
     1. Podemos passar um state ao <Link /> que armazenara o objeto que quisermos
