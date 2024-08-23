06a735ca7020041cfd304aed21bb8069b9f75102
Emanuelprime <emanuelll3000@gmail.com>
2024-08-23 13:55:54 -0300

    style-home

--- receitas-app/src/Pages/Home.js
+++ receitas-app/src/Pages/Home.js
@@ -1,19 +1,30 @@
-import Recipe from '../Components/Recipe';
 import './Styles/Home.css';
 
 export default function Recipes({ data }) {
-    return(
-        <div>
-            {data.map(objectInfo => (
-                <div className="recipe-container" key={objectInfo.id}>
-                    <img src={objectInfo.img} alt={objectInfo.titulo} />
-                    <Recipe recipeObject={objectInfo} />
-                </div>
-            ))}
+  return (
+    <div className="recipe-list">
+      {data.map(objectInfo => (
+        <div className="recipe-item" key={objectInfo.id}>
+          <img
+            src={objectInfo.img}
+            alt={objectInfo.titulo}
+            className="recipe-image"
+          />
+          <div className="recipe-info">
+            <h2 className="recipe-title">{objectInfo.titulo}</h2>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
+/*
+    Anotacoes que achei relevante
+    1. Podemos passar um state ao <Link /> que armazenara o objeto que quisermos
+    2. Em uma arrow function, podemos utilizar () ao inves de {} na expressao () => {}, ficando assim () => (), removendo-se a obrigatoriedade de um return
+    3. Para conseguir usar um template literal em uma tag JSX, usamos {}
+*/
 /*
     Anotacoes que achei relevante
     1. Podemos passar um state ao <Link /> que armazenara o objeto que quisermos
